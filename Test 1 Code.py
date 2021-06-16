2dbefb307bc5073f1f22772d2325e4931990c786
MbaliGit <85889148+MbaliGit@users.noreply.github.com>
2021-06-16 19:47:27 +0200

    Add files via upload

---
+++ Test 1 Code.py
@@ -0,0 +1,9 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World!")
+Hello World!
+>>> name = input("Enter name: ")
+Enter name: John
+>>> print("Hello "+ name)
+Hello John
+>>> 
